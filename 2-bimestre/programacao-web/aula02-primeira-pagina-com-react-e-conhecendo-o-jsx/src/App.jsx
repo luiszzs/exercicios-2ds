76e502e529aea9f2fb0e3d9cde6f9f6c116e1ef2
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2026-04-27 21:44:16 +0000

    feito

--- 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
+++ 2-bimestre/programacao-web/aula02-primeira-pagina-com-react-e-conhecendo-o-jsx/src/App.jsx
@@ -1,9 +1,11 @@
 export default function App(){
   const materia = "Fisga"
+  const filme = "The Batman"
   return(
     <div>
       <h1>Luis Guilherme</h1>
       <p>Matéria favorita é: {materia}</p>
+      <p>Filme favorito é: {filme}</p>
     </div>
   )
 }
