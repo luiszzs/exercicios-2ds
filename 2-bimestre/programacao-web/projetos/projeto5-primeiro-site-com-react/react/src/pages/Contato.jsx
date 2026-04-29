b37e901c281ef7e58ebb7b6429a00f2b00ca9536
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2026-04-29 17:27:58 +0000

    feita

--- 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/Contato.jsx
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/pages/Contato.jsx
@@ -2,7 +2,8 @@ export default function Contato(){
     return(
         <div>
             <h2>Contato</h2>
-            <p>contato@site.com ou (85)9283753</p>
+            <p>contato@site.com</p>
+            <p>(85)9283753</p>
         </div>
     )
 }
