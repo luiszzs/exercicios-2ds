1e5c4ac3810b4bb2a248a00764065bd02d1dd39c
Luis Guilherme <luis.gomes76@aluno.ce.gov.br>
2026-08-19 11:52:31 +0000

    acabei aula 9 e 10

---
+++ 3-bimestre/orientacao-a-objetos/aula09/index.js
@@ -0,0 +1,16 @@
+class Usuario{
+    #nome
+
+    constructor(nome){
+        this.#nome = nome
+    }
+
+    get nome(){
+        return this.#nome
+    }
+}
+
+const usuario = new Usuario("John")
+console.log(usuario.nome)
+
+module.exports = usuario
